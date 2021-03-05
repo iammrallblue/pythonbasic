f85f0fba648d6d36bb32d16e8bb032f391a54fda
joh <iammrallblue@gmail.com>
2021-03-05 14:28:49 -0500

    day05

--- day05/demo04_lambda_expression.py
+++ day05/demo04_lambda_expression.py
@@ -9,7 +9,11 @@
             2. can have many arguments.
             3. the expression after colon is unique. it's not an argument
             4. lambda function has a default return.
+            5. lambda function can ONLY have one expression after colon.
+                the designation of lambda function is ONLY for a simple function requirement.
+            5. lambda function is for simple logic, not for complex logic.
 """
+
 
 # a standard function in python
 
@@ -28,14 +32,26 @@ def add_numbers(x, y):
 print(add_numbers(1, 2))  # 3
 
 # lambda function, convert a standard function into lambda function
-
-
-# def L(x, y): return x+y 
-L = lambda x, y: x+y
-
+# def L(x, y): return x+y (Not recommended)
+L = lambda x, y: x + y
 
 # call lambda function
 print(L(3, 4))  # 7
 
-result = lambda x,y,z: x*y*z
-print(result(1,2,3))
+result = lambda x, y, z: x * y * z
+print(result(1, 2, 3))
+
+# lambda function substitutes if else statement
+age = 12
+print("You are adult." if age > 18 else "You are just a kid.")
+
+# by lambda function
+lambda_func = lambda x, y: x if x > y else y
+print(lambda_func(10, 9))
+print(type(lambda_func)) # <class 'function'>
+
+# by lambda function 2
+lambda_func2 = (lambda x, y: x if x > y else y)(19, 15) # directly call lambda function
+# 1. (lambda x, y: x if x > y else y) this is the object of an anonymous function
+# 2. (19,15) call the anonymous function directly after the function
+print(lambda_func2)
