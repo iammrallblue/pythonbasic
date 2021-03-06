1b095c1a0606f952e75b0d917eecaa4546612ccc
joh <iammrallblue@gmail.com>
2021-03-06 13:15:23 -0500

    day06

---
+++ day07/demo01_oop.py
@@ -0,0 +1,19 @@
+"""
+    OOP:
+        Object-Oriented Programming (OOP) :
+            An object has two characteristics: (a parrot is an object )
+            attributes
+                name, age, color, gender, as attributes
+            behavior
+                singing, dancing, as behavior
+        Class:
+            a blueprint of an object.
+            Syntax:
+                class Parrot:
+                pass
+        Object:
+            An object (instance) is an instantiation of a class.
+            Syntax:
+                obj = Parrot:
+                    obj is an object of class Parrot.
+"""
