adedb7a41ac5fb32a42f64aa7c6dd35477438ef7
joh <iammrallblue@gmail.com>
2021-03-18 09:32:30 -0400

    day01 updated

--- day01/demo09_operators.py
+++ day01/demo09_operators.py
@@ -14,10 +14,25 @@
       Assignment Operators:
         1. =, +=, -=, *=, /=
         2. %=, **=, //=
-      Logic Operators:
-        1. and
-        2. or
-        3. not
+
+
+      Logic operators
+        and
+            return True if both statements are True
+        or
+            return True if one of statements is True
+        not
+            reverse the result, returns False if the result is True
+      Identity operators:
+        is
+            return True if both variables are the same object
+        is not
+            return True if both variables are NOT the same object
+      Membership operators:
+        in
+            returns True if a sequence with the specified value is present in the object (x in y)
+        not in
+            returns True if a sequence with the specified value is NOT present in the object ( x not in y)
 """
 # declare two variables x, and y.
 x = 10
@@ -39,6 +54,9 @@ print(x_to_y)  # 10^2 = 100
 flo_div = y // x  # 2/10 = 0
 print("floor division: ", flo_div)
 
+#
+mod_div = y / x
+print("y/x:", mod_div)  # 0.2
 """
     Comparison Operators:
         1. ==
@@ -84,3 +102,26 @@ print(x + y < z or x * z > y)  # True
 x **= 10
 print(x)  # 3^10 =59049, x = 59049
 # print("x%d **= 10:" % (x), x)
+
+
+# logic operators: (and, or, not)
+x = 5
+# print(x > 1 and x < 10)
+print(1 < x < 10)  # True, both are True
+
+y = "hello"
+print(y and 1 == 2)
+print(y or 1 == 2)
+
+print(x > 1 or x < 1)  # True, one of both is True
+
+print(not (x > 1 or x < 0))  # False, because reverse the True result
+
+# identity operators: (is, is not)
+print(x is y)  # False, different objects
+print(x is not y)  # True
+
+# membership Operators (in, not in)
+print("h" in y)  # True
+print("h" not in y)  # False
+print("h,l" in y) # False
