5ca429b30b93af852bdf24ebf4334edc1518571b
joh <iammrallblue@gmail.com>
2021-03-20 22:29:30 -0400

    day01

--- day01/demo07_condition_loop_statement.py
+++ day01/demo07_condition_loop_statement.py
@@ -21,10 +21,17 @@ while True:
         print(type(user))  # input will always be str type
         if user == "":
             print("user name can't be EMPTY")
+            continue
             pass
         elif user == " ":
             print("user name can't be SPACE")
+            continue
         print("Finish")
 
 print("out of if statement.")
 
+print(len(" 44S$   "))  # length of the string is 8
+
+print(not 0)  # True
+print(not "0")  # False
+print(0)
