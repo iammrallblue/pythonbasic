b0449c0bb86615a4a71925a7867a304a5860420e
joh <iammrallblue@gmail.com>
2021-05-22 12:07:40 -0400

    day03

--- day03/demo17_more_dict.py
+++ day03/demo17_more_dict.py
@@ -22,61 +22,75 @@
                     my_dict.setdefault("key","value")
 
 """
-my_dict = {}
-
-# add key and value
-my_dict["Name"] = "Abby"
-print(my_dict)  # {'Name': 'Abby'}
-
-# replace switch case statement
-my_dict2 = {
-    "0": "Zero",
-    "1": "One",
-    "2": "Two"
-}
-
-# arg = input(">>:")
+# my_dict = {}
 #
-# get_value = my_dict2.get(arg, "nothing")
-# print(get_value)
+# # add key and value
+# my_dict["Name"] = "Abby"
+# print(my_dict)  # {'Name': 'Abby'}
+#
+# # replace switch case statement
+# my_dict2 = {
+#     "0": "Zero",
+#     "1": "One",
+#     "2": "Two"
+# }
+#
+# # arg = input(">>:")
+# #
+# # get_value = my_dict2.get(arg, "nothing")
+# # print(get_value)
+#
+# while True:
+#
+#     arg = input(">>:")  # input key to find value
+#     if arg == "Exit" or arg == "exit":
+#         # exit()
+#         break
+#         pass
+#     else:
+#         # get()
+#         get_value = my_dict2.get(arg, "nothing")  # get() function finds the value by key
+#         print(get_value)
+#         pass
+#     pass
+# pass
+#
+# # setdefault() function
+# my_dict3 = {"a": "1"}
+#
+# return_value = my_dict3.setdefault("a")  # parameter is the key in the dict
+# print(return_value)  # return value by key, the value is 1
+# print(my_dict3)  # print out my_dict3, {'a': '1'}
+#
+# # add key and value if don't exist
+# my_dict3.setdefault("b", "Bella")
+# print(my_dict3)  # {'a': '1', 'b': 'Bella'}
+#
+# # list as value in dict
+# list_as_value = my_dict3.setdefault("list", [])
+# print(list_as_value)  # [], return an empty list
+# print(type(my_dict3["list"]))  # <class 'list'>
+# print(id(my_dict3["list"]))  # 4441325888
+#
+# print(my_dict3)  # {'a': '1', 'b': 'Bella', 'list': []}
+#
+# # # add elements to the list in my_dict3
+# add_elem = my_dict3.setdefault("list", []).append(1)
+# print(my_dict3)  # {'a': '1', 'b': 'Bella', 'list': [1]}
+#
+# listAsValue = []
+# print(type(listAsValue))  # <class 'list'>
+#
+# person = {'name': 'lizhong', 'age': '26', 'city': 'BeiJing', 'blog': 'www.jb51.net'}
+#
+# for x, y in person.items():
+#     print(x, y)
 
-while True:
+list_a = [{"name": "推荐食谱", "1": "症状", "name1": "浑身忽冷忽热"}, {"name": "绿豆薏米饭"}, {"name": "芝麻"}]
 
-    arg = input(">>:")  # input key to find value
-    if arg == "Exit" or arg == "exit":
-        # exit()
-        break
+res = [item[key] for item in list_a for key in item]
+print(res)
+for item in list_a:
+    for key in item:
+        print(item[key])
         pass
-    else:
-        # get()
-        get_value = my_dict2.get(arg, "nothing")  # get() function finds the value by key
-        print(get_value)
-        pass
-    pass
-pass
-
-# setdefault() function
-my_dict3 = {"a": "1"}
-
-return_value = my_dict3.setdefault("a")  # parameter is the key in the dict
-print(return_value)  # return value by key, the value is 1
-print(my_dict3)  # print out my_dict3, {'a': '1'}
-
-# add key and value if don't exist
-my_dict3.setdefault("b", "Bella")
-print(my_dict3)  # {'a': '1', 'b': 'Bella'}
-
-# list as value in dict
-list_as_value = my_dict3.setdefault("list", [])
-print(list_as_value)  # [], return an empty list
-print(type(my_dict3["list"]))  # <class 'list'>
-print(id(my_dict3["list"]))  # 4441325888
-
-print(my_dict3)  # {'a': '1', 'b': 'Bella', 'list': []}
-
-# # add elements to the list in my_dict3
-add_elem = my_dict3.setdefault("list", []).append(1)
-print(my_dict3)  # {'a': '1', 'b': 'Bella', 'list': [1]}
-
-listAsValue = []
-print(type(listAsValue))  # <class 'list'>
