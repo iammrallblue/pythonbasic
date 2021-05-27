442726043eac1cd8f645a148cc34f5be9ee51f17
joh <iammrallblue@gmail.com>
2021-05-26 23:12:49 -0400

    practice

--- practice/guess_random_number.py
+++ practice/guess_random_number.py
@@ -5,17 +5,30 @@
 
 """
 import random
-# random a number then assign to ran_number variable
-ran_number = random.randint(0, 10)
-print(ran_number)
-# check random number
 
-user_guess = int(input("what is your guessing number: "))
-if ran_number > user_guess:
-    print("your guessing number is small.")
-elif ran_number < user_guess:
-    print("your guessing number is big.")
-elif ran_number == user_guess:
-    print("bingo, you won.")
-else:
-    print("something wrong.")
+# # random a number then assign to ran_number variable
+# ran_number = random.randint(0, 10)
+# print(ran_number)
+# # check random number
+#
+# user_guess = int(input("what is your guessing number: "))
+# if ran_number > user_guess:
+#     print("your guessing number is small.")
+# elif ran_number < user_guess:
+#     print("your guessing number is big.")
+# elif ran_number == user_guess:
+#     print("bingo, you won.")
+# else:
+#     print("something wrong.")
+
+
+random_number = random(1, 100)
+counter = 0
+while True:
+    counter += 1
+    guess_number = int(input("Input your guessing number."))
+    # comparing random number and guessing number by if... else statement
+    if guess_number < random_number:
+        print("Guessing number is too small.")
+    elif guess_number > random_number:
+        print("Guessing number is too big.")
